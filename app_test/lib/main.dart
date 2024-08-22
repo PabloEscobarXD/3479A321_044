f33eea5c5a4ed43cd2f134a532b97ba1fbf6584d
pabrojas_utalca <65563222+PabloEscobarXD@users.noreply.github.com>
2024-08-22 10:47:54 -0400

    Botones añadidos
    Boton de disminuir y reiniciar contador

--- app_test/lib/main.dart
+++ app_test/lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'CONTADOR'),
     );
   }
 }
@@ -65,6 +65,26 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+    });
+  }
+  void _decreasecounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter--;
+    });
+  }
+  void _restartcounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter = 0;
     });
   }
 
@@ -120,6 +140,13 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+      persistentFooterButtons: [
+        ElevatedButton(onPressed: _decreasecounter, 
+        child: Icon(Icons.remove)), 
+        ElevatedButton(onPressed: _restartcounter, 
+        child: Icon(Icons.restart_alt))
+      ],
+      
     );
   }
 }
