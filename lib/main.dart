fed2c5fbaa5c1505c097f0ebb9cd81db3c949f5e
PabloEscobarXD <pescobar20@alumnos.utalca.cl>
2025-04-27 20:52:36 -0400

    Laboratorio 5

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purpleAccent),
         fontFamily: 'IBMPlexMono'
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'HomePage'),
     );
   }
 }
